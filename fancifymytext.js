8fa33b7d8ae68d33dd1f92080d6f03f1d21ce054
iamdeeshadow <80650349+iamdeeshadow@users.noreply.github.com>
2022-12-06 20:21:26 -0800

    created webpage

---
+++ fancifymytext.js
@@ -0,0 +1,33 @@
+function bigger() 
+{
+    var text = document.getElementById("inputText");
+    text.style.fontSize = "24pt";
+    alert("Hello, world!");
+}
+
+function fancySchmancy() 
+{
+var text = document.getElementById("inputText");
+text.style.fontWeight = "bold";
+text.style.color= "green";
+text.style.textDecoration= "underline";
+alert("Fancy Shmancy Text :D");
+}
+
+function boringBetty() 
+{
+var text = document.getElementById("inputText");
+text.style.fontSize = "initial";
+text.style.fontWeight = "normal";
+text.style.color = "black";
+text.style.textDecoration= "none";
+alert("Boring Betty Text :(");
+}
+
+function addMoo() 
+{
+var text = document.getElementById("inputText");
+text.style.textTransform = "uppercase";
+text.value = text.value.split(".").join("-Moo.");
+alert("MOOs added after every sentence indicated by a period (.)")
+}
